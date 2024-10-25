e3e56c6824a1e6483894d85ac9f248b12a4df15e
BaiForce <tomketpunks@gmail.com>
2024-10-25 15:10:25 +0800

    Update Navbar

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,20 +1,39 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
+
 const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="">
+    <div>
       <div
-        className="flex flex-col gap-[4.5] cursor-pointer"
+        className="flex flex-col gap-1 cursor-pointer"
         onClick={() => setIsOpen((prev) => !prev)}
       >
-        <div className="w-6 h-1 bg-blue-600 rounded-sm" />
-        <div className="w-6 h-1 bg-blue-600 rounded-sm" />
-        <div className="w-6 h-1 bg-blue-600 rounded-sm" />
+        {/* Bar atas */}
+        <div
+          className={`w-6 h-1 bg-blue-600 rounded-sm transition-transform duration-300 ${
+            isOpen ? "rotate-45 translate-y-2" : ""
+          }`}
+        />
+        {/* Bar tengah */}
+        <div
+          className={`w-6 h-1 bg-blue-600 rounded-sm transition-opacity duration-300 ${
+            isOpen ? "opacity-0" : ""
+          }`}
+        />
+        {/* Bar bawah */}
+        <div
+          className={`w-6 h-1 bg-blue-600 rounded-sm transition-transform duration-300 ${
+            isOpen ? "-rotate-45 -translate-y-2" : ""
+          }`}
+        />
       </div>
+
+      {/* Menu dropdown */}
       {isOpen && (
-        <div className="flex flex-col gap-4 mt-4">
+        <div className="absolute top-24 left-0 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10">
           <Link href="/">Home</Link>
           <Link href="/">Friend</Link>
           <Link href="/">Groups</Link>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,34 @@
 import MobileMenu from "./MobileMenu";
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
       {/* LEFT  */}
-      <div className="">
+      <div className="md:hidden lg;block">
         <Link href="/" className="font-bold text-xl text-blue-600">
           BAISOCIAL
         </Link>
       </div>
       {/* CENTER  */}
-      <div className="hidden"></div>
+      <div className="hidden md:flex">
+        {/* LINKS */}
+        <div className="">
+          <Link href="/" className="flex gap-6 text-gray-600">
+            <Image src="/home.png" alt="HomePage" width={16} height={16} />
+            <span>HomePage</span>
+          </Link>
+          <Link href="/" className="flex gap-2">
+            <Image src="/friends.png" alt="Friends" width={16} height={16} />
+            <span>Friends</span>
+          </Link>
+          <Link href="/" className="flex gap-2">
+            <Image src="/stories.png" alt="Stories" width={16} height={16} />
+            <span>Home Page</span>
+          </Link>
+        </div>
+      </div>
       {/* RIGHT  */}
       <div className="">
         <MobileMenu />
