e411d6045fb71f4441ac6a3df87abd4f8b9c40d5
BaiForce <tomketpunks@gmail.com>
2024-11-14 16:28:58 +0800

    update post

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const AddPost = () => {
   return (
-    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+    <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
       {/* AVATAR */}
       <Image
         src="https://images.pexels.com/photos/1796715/pexels-photo-1796715.jpeg?auto=compress&cs=tinysrgb&w=800"
---
+++ src/components/Post.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+
+const Post = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      {/* USER  */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src="https://images.pexels.com/photos/26087617/pexels-photo-26087617/free-photo-of-the-hike-to-hoodoos-in-banff-national-park.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt=""
+            width={40}
+            height={40}
+            className="w-10 h-10 rounded-full"
+          />
+          <span className="font-medium">Gerson Wissaluno</span>
+        </div>
+        <Image src="/more.png" alt="" width={16} height={16} />
+      </div>
+      {/* DEXC  */}
+      <div className="flex flex-col gap-4">
+        <div className="w-full min-h-96 relative">
+          <Image
+            src="https://images.pexels.com/photos/1978126/pexels-photo-1978126.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <p>
+          Tahun ini, untuk pertama kalinya, aku merasakan salju di luar negeri.
+          Suasana yang begitu magis, saat butiran salju pertama kali menyentuh
+          kulit, seolah dunia berhenti sejenak dan segala yang ada di sekitar
+          terasa begitu berbeda. Suara langkah yang tertinggal di atas salju,
+          udara yang lebih dingin dari biasanya, semuanya membawaku ke dalam
+          pengalaman yang tak terlupakan.
+        </p>
+      </div>
+      {/* INTERACTION  */}
+      <div className="flex items-center justify-between text-sm">
+        <div className="flex gap-8">
+          <div className="flex items-center gap-4 bg-slate-100 px-4 py-2 rounded-xl"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
