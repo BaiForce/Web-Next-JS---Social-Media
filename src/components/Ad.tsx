0d9ed7c6005706fc91f44d26f4d469b6307d95e3
BaiForce <tomketpunks@gmail.com>
2024-11-15 12:10:27 +0800

    Tambah Fitur Friends Req dan Birtdays

---
+++ src/components/Ad.tsx
@@ -0,0 +1,5 @@
+const Ad = ({ size }: { size: "sm" | "md" | "lg" }) => {
+  return <div className="p-4 rounded-lg bg-white shadow-md text-sm">Ad</div>;
+};
+
+export default Ad;
--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,5 +1,15 @@
-const RightMenu = () => {
-  return <div className="">RightMenu</div>;
+import Ad from "./Ad";
+import Birthdays from "./Birthdays";
+import Friends from "./Friends";
+
+const RightMenu = ({ userId }: { userId?: string }) => {
+  return (
+    <div className="flex flex-col gap-6">
+      <Friends />
+      <Birthdays />
+      <Ad size="md" />
+    </div>
+  );
 };
 
 export default RightMenu;
