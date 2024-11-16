752750dbf953f32866225bb1bdd4d07d86ab4d24
BaiForce <tomketpunks@gmail.com>
2024-11-16 12:17:38 +0800

    Update Fitur Followers

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+
+const ProfileCard = () => {
+  return (
+    <div className="p-4 bg-white shadow-md rounded-lg text-sm flex flex-col gap-6">
+      <div className="h-20 relative">
+        <Image
+          src={
+            "https://images.pexels.com/photos/21314033/pexels-photo-21314033/free-photo-of-sailboat-anchored-on-charles-river-in-boston.jpeg?auto=compress&cs=tinysrgb&w=800"
+          }
+          alt=""
+          fill
+          className="rounded-md object-cover"
+        />
+        <Image
+          src={
+            "https://images.pexels.com/photos/18394016/pexels-photo-18394016/free-photo-of-tower-of-memorial-hall-in-cambridge-in-usa.jpeg?auto=compress&cs=tinysrgb&w=800"
+          }
+          alt=""
+          width={48}
+          height={48}
+          className="rounded-full w-12 h-12 absolute left-0 right-0 mx-auto -bottom-6 ring-1 ring-white z-10 object-cover"
+        />
+      </div>
+      <div className="h-20 flex flex-col items-center ">
+        <span className="font-semibold ">Raul Sanjaya </span>
+        <div className="flex items-center gap-4 ">
+          <div className="flex ">
+            <Image
+              src={
+                "https://images.pexels.com/photos/18394016/pexels-photo-18394016/free-photo-of-tower-of-memorial-hall-in-cambridge-in-usa.jpeg?auto=compress&cs=tinysrgb&w=800"
+              }
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full w-3 h-3 "
+            />
+            <Image
+              src={
+                "https://images.pexels.com/photos/18394016/pexels-photo-18394016/free-photo-of-tower-of-memorial-hall-in-cambridge-in-usa.jpeg?auto=compress&cs=tinysrgb&w=800"
+              }
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full w-3 h-3 "
+            />
+            <Image
+              src={
+                "https://images.pexels.com/photos/18394016/pexels-photo-18394016/free-photo-of-tower-of-memorial-hall-in-cambridge-in-usa.jpeg?auto=compress&cs=tinysrgb&w=800"
+              }
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full w-3 h-3 "
+            />
+          </div>
+          <span className="text-xs text-gray-500 ">500 Followers</span>
+        </div>
+        <button className="bg-blue-500 text-white text-xs mt-4 p-2 rounded-md ">My Profile</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
