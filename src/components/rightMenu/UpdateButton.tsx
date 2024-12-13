71151987e22ff8bf7ccf7ef2ec0747161f5bac9e
BaiForce <tomketpunks@gmail.com>
2024-12-13 14:25:52 +0800

    update

--- src/components/rightMenu/UpdateButton.tsx
+++ src/components/rightMenu/UpdateButton.tsx
@@ -7,7 +7,7 @@ const UpdateButton = () => {
 
   return (
     <button
-      className="bg-blue-500 p-2 mt-2 text-white disabled:opacity-50 disabled:cursor-not-allowed"
+      className="bg-blue-500 p-2 mt-2 text-white rounded-xl disabled:opacity-50 disabled:cursor-not-allowed"
       disabled={pending}
     >
       {pending ? "Updating..." : "Update"}
