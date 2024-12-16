215eba1e4210b8dc8b8158d51e3cb58d27b6f3aa
BaiForce <tomketpunks@gmail.com>
2024-12-16 13:51:24 +0800

    update post

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,36 @@
-import MobileMenu from "./MobileMenu";
 import Link from "next/link";
+import MobileMenu from "./MobileMenu";
 import Image from "next/image";
 import {
   ClerkLoaded,
   ClerkLoading,
   SignedIn,
   SignedOut,
-  SignIn,
   UserButton,
 } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
-      {/* LEFT  */}
-      <div className="md:hidden lg;block w-[20%]">
+      {/* LEFT */}
+      <div className="md:hidden lg:block w-[20%]">
         <Link href="/" className="font-bold text-xl text-blue-600">
-          BAISOCIAL
+          BAIFORCE SOCIAL
         </Link>
       </div>
-      {/* CENTER  */}
-      <div className="hidden md:flex w-[50%] text-sm">
+      {/* CENTER */}
+      <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
         {/* LINKS */}
         <div className="flex gap-6 text-gray-600">
           <Link href="/" className="flex items-center gap-2">
             <Image
               src="/home.png"
-              alt="HomePage"
+              alt="Homepage"
               width={16}
               height={16}
               className="w-4 h-4"
             />
-            <span>HomePage</span>
+            <span>Homepage</span>
           </Link>
           <Link href="/" className="flex items-center gap-2">
             <Image
@@ -51,19 +50,27 @@ const Navbar = () => {
               height={16}
               className="w-4 h-4"
             />
-            <span>Home Page</span>
+            <span>Stories</span>
           </Link>
         </div>
+        <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent outline-none"
+          />
+          <Image src="/search.png" alt="" width={14} height={14} />
+        </div>
       </div>
-      {/* RIGHT  */}
+      {/* RIGHT */}
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
         <ClerkLoading>
-          <div className='class="inline-block h-8 w-8 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] text-surface opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite] dark:text-white"'></div>
+          <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
         </ClerkLoading>
         <ClerkLoaded>
           <SignedIn>
             <div className="cursor-pointer">
-              <Image src="/people.png" alt="" width={20} height={20} />
+              <Image src="/people.png" alt="" width={24} height={24} />
             </div>
             <div className="cursor-pointer">
               <Image src="/messages.png" alt="" width={20} height={20} />
@@ -74,8 +81,8 @@ const Navbar = () => {
             <UserButton />
           </SignedIn>
           <SignedOut>
-            <div className="flex items-center gap-2">
-              <Image src="/login.png" alt="login icon" width={20} height={20} />
+            <div className="flex items-center gap-2 text-sm">
+              <Image src="/login.png" alt="" width={20} height={20} />
               <Link href="/sign-in">Login/Register</Link>
             </div>
           </SignedOut>
--- src/components/feed/Post.tsx
+++ src/components/feed/Post.tsx
@@ -1,91 +1,57 @@
 import Image from "next/image";
 import Comments from "./Comments";
+import { Post as PostType, User } from "@prisma/client";
+import PostInteraction from "./PostInteraction";
 
-const Post = () => {
+type FeedPostType = PostType & { user: User } & {
+  likes: [{ userId: string }];
+} & {
+  _count: { comments: number };
+};
+const Posts = ({ post }: { post: FeedPostType }) => {
   return (
     <div className="flex flex-col gap-4">
       {/* USER  */}
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4">
           <Image
-            src="https://images.pexels.com/photos/26087617/pexels-photo-26087617/free-photo-of-the-hike-to-hoodoos-in-banff-national-park.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            src={post.user.avatar || "/noAvatar.png"}
             alt=""
             width={40}
             height={40}
             className="w-10 h-10 rounded-full"
           />
-          <span className="font-medium">Gerson Wissaluno</span>
+          <span className="font-medium">
+            {post.user.name && post.user.surname
+              ? post.user.name + "" + post.user.surname
+              : post.user.username}
+          </span>
         </div>
         <Image src="/more.png" alt="" width={16} height={16} />
       </div>
       {/* DEXC  */}
       <div className="flex flex-col gap-4">
-        <div className="w-full min-h-96 relative">
-          <Image
-            src="https://images.pexels.com/photos/1978126/pexels-photo-1978126.jpeg?auto=compress&cs=tinysrgb&w=800"
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <p>
-          Tahun ini, untuk pertama kalinya, aku merasakan salju di luar negeri.
-          Suasana yang begitu magis, saat butiran salju pertama kali menyentuh
-          kulit, seolah dunia berhenti sejenak dan segala yang ada di sekitar
-          terasa begitu berbeda. Suara langkah yang tertinggal di atas salju,
-          udara yang lebih dingin dari biasanya, semuanya membawaku ke dalam
-          pengalaman yang tak terlupakan.
-        </p>
+        {post.img && (
+          <div className="w-full min-h-96 relative">
+            <Image
+              src={post.img}
+              alt=""
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+        )}
+        <p>{post.desc}</p>
       </div>
       {/* INTERACTION  */}
-      <div className="flex items-center justify-between text-sm my-4">
-        <div className="flex gap-8">
-          <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-            <Image
-              src="/like.png"
-              alt=""
-              width={16}
-              height={16}
-              className="curson-pointer"
-            />
-            <span className="text-gray-300">|</span>
-            <span className="text-gray-500">
-              135 <span className="hidden md:inline"> Likes</span>
-            </span>
-          </div>
-          <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-            <Image
-              src="/comment.png"
-              alt=""
-              width={16}
-              height={16}
-              className="curson-pointer"
-            />
-            <span className="text-gray-300">|</span>
-            <span className="text-gray-500">
-              135 <span className="hidden md:inline"> Comments</span>
-            </span>
-          </div>
-        </div>
-        <div className="">
-          <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-            <Image
-              src="/share.png"
-              alt=""
-              width={16}
-              height={16}
-              className="curson-pointer"
-            />
-            <span className="text-gray-300">|</span>
-            <span className="text-gray-500">
-              135 <span className="hidden md:inline"> Share</span>
-            </span>
-          </div>
-        </div>
-      </div>
+      <PostInteraction
+        postId={post.id}
+        likes={post.likes.map((like) => like.userId)}
+        commentNumber={post._count.comments}
+      />
       <Comments />
     </div>
   );
 };
 
-export default Post;
+export default Posts;
